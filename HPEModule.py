0bb15e883445a0fe840ae77c18c2f907a2746dc0
amodi01 <amitmodi.79@gmail.com>
2021-04-14 00:53:38 +0530

    Initial commit

---
+++ HPEModule.py
@@ -0,0 +1,15 @@
+import cv2
+import mediapipe
+
+class poseDetector():
+
+    def __init__(self, mode=False,upperBody=False,smooth=True,detectConfidence=0.5,trackConfidence=0.5):
+        self.Mode=mode
+        self.UpperBody=upperBody
+        self.Smooth=smooth
+        self.DetectConfidence=detectConfidence
+        self.TracKConfidence=trackConfidence
+def main():
+
+if __name__=="__main__":
+    main()
